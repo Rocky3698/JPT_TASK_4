78fc3a0d229a4291189d057847fd1b85af840d0f
Rocky Chowdhury <rocky20809@gmail.com>
2024-05-14 21:03:11 +0600

    updated

---
+++ function_overloading.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class A{
+    public:
+    void show(int a){
+        cout<<"given "<<a<<endl;
+    }
+    void show(float a){
+        cout<<"given "<<a<<endl;
+    }
+    void show(int a, int b){
+        cout<<"given "<<a+b<<endl;
+    }
+};
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+
+    return 0;
+}
---
+++ function_overloading.py
@@ -0,0 +1,7 @@
+class A:
+    def show(a):
+        print("given", a)
+    def show(a,b):
+        print("given", a,b)
+    def show(a,b,c):
+        print("given", a,b,c)
